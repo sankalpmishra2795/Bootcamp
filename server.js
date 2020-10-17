e2244239357ecc1ddcf8c5df3358ead2f764f5bc
sankalpmishra2795 <sankalpmishra2795@gmail.com>
2020-10-17 22:53:10 +0530

    Router & middleware

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const express = require("express")
 
 const dotenv = require ("dotenv")
 
+const morgan = require("morgan")
+// route file
+const bootcamps = require('./router/bootcamps')
+
 // Load the config file(env vars)
 dotenv.config({path:"./config/config.env"})
 
 const app = express();
+// dev logging ,middelware
+if(process.env.NODE_ENV === "devlopment"){
+    app.use(morgan('dev'))
+}
+
+
+// mount routs
+app.use('/api/v1/bootcamps',bootcamps)
 
 const  PORT = process.env.PORT || 5000
 
