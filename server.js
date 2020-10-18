7efd10d3ef5cf3bc9d2c027679e13b6646b22674
sankalpmishra2795 <sankalpmishra2795@gmail.com>
2020-10-18 20:16:46 +0530

    Modals

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-
 const express = require("express")
-
 const dotenv = require ("dotenv")
-
 const morgan = require("morgan")
-// route file
-const bootcamps = require('./router/bootcamps')
+const connectDB = require('./config/db')
 
 // Load the config file(env vars)
 dotenv.config({path:"./config/config.env"})
+
+// Connect database
+connectDB()
+
+// route file
+const bootcamps = require('./router/bootcamps')
+
 
 const app = express();
 // dev logging ,middelware
@@ -22,4 +25,13 @@ app.use('/api/v1/bootcamps',bootcamps)
 
 const  PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// handle promise rejection
+
+process.on("unhandledRejection", (err,promise) =>{
+    console.log(`Error: ${err.message}`)
+
+    // Close server & exit process
+    server.close(()=>process.exit(1))
+})
